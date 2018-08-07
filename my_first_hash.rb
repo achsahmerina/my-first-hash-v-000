fe0b63ce63662035192afd21c279690ea8e5038b
Achsah Abraham <achsahmp@gmail.com>
2018-08-07 22:19:38 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,7 +17,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+                 return shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -30,7 +30,7 @@ def adding
                   "muskets" => 2,
                   "gun powder" => 4
                  }
-                 return shipping_manifest["oil paintings"]
+              
   # add 2 muskets to the shipping_manifest hash below
 
 

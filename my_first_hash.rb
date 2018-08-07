6b682e47ed075a61221fbc9c05c898e37dca64da
Achsah Abraham <achsahmp@gmail.com>
2018-08-07 22:17:49 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,6 +30,7 @@ def adding
                   "muskets" => 2,
                   "gun powder" => 4
                  }
+                 return shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
